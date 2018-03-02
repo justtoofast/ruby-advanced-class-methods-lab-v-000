f5fa2edb2f38844c7eeed51d8e953f1ea8fe0d84
Trac Nguyen <nguyendtrac@gmail.com>
2018-03-02 18:44:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,12 +33,6 @@ class Song
   end
 
   def self.find_or_create_by_name(song_name)
-    song = self.new
-    song.name = song_name
-    @@all << song
-
-    @@all.detect{|song| song.name == song_name}
-
-    song
+    self.find_by_name(song_name)
   end
 end
